7ef3bd2dfd3459732eddc34cbdcd9b802fa0c633
Tuzhiqiang1996 <1602072414@qq.com>
2021-02-04 18:01:25 +0800

    [new]上传后端代码

---
+++ SpringBootJava/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.controller;
+
+
+import com.example.common.lang.Result;
+import com.example.entity.User;
+import com.example.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author
+ * @since 2021-01-29
+ */
+
+/**
+ * 测试结果
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequiresAuthentication
+    @GetMapping("/index")
+    public Result index() {
+        User user = userService.getById(1L);
+        return Result.succ(user);
+    }
+
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user) {
+        return Result.succ(user);
+    }
+
+
+}
